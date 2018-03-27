6913a102254ab386fa6031abc319c7c3b165308f
efrechon <edgar.frechon@free.fr>
2018-03-27 18:59:53 +0200

    HTML & CSS Trainning

---
+++ AccueilType/menu.php
@@ -0,0 +1,20 @@
+<nav>
+    <ul>
+        <li>Acceuil</li>
+        <li>Vue d'ensemble</li>
+        <li>Mes capteurs</li>
+        <li>Mes actionneurs</li>
+        <li>Scénarios</li>
+        <li>Notifications</li>
+        <li>Gérer les utilisateurs</li>
+        <li>Gérer les maisons</li>
+        <li>Informations</li>
+        <li>SAV</li>
+        <br><br><br><br><br><br><br>
+        <li class="other">FAQ</li>
+        <li class="other">A propos</li>
+        <li class="other">Nous contacter</li>
+        <li class="other">CGU</li>
+        <li class="other">Mentions Légales</li>
+    </ul>
+</nav>
