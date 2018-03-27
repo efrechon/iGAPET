75a5d004b2ebc02e1ad2ed08a2b7bc92274d74b5
gregoireFessard <37829621+gregoireFessard@users.noreply.github.com>
2018-03-27 13:26:28 +0200

    Add files via upload

---
+++ Greg/Config.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "IGAPET";
+
+try{
+    $db = new PDO('mysql:host='.$host.';dbname='.$dbname,$user,$pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(Exception $e)
+{
+    echo 'Erreur : '.$e->getMessage().'<br />';
+    echo 'N° : '.$e->getCode();
+}
