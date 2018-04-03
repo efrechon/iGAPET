974ac8daf02fbad541fac8d13efe6696e0e79048
efrechon <edgar.frechon@free.fr>
2018-04-03 20:29:27 +0200

    Suppression pour organisation 1

--- AccueilType/header.php
+++
@@ -1,7 +0,0 @@
-<header>
-    <img id="logo" src="Logo.png" title="Logo">
-    <h3 id="nomSite">iGAPET</h3>
-    <h2 id="nomPage"><?php echo "Accueil"; ?></h2>
-    <p id="nomClient" title="Mon profil"><?php echo "Nom client"; ?></p>
-    <a href="connexion.php"><img class="off" src="Power.png" src="Deconnexion"></a>
-</header>
