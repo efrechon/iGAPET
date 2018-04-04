06329f32e9e7c6480c3e64b80b7f1f200ee8dd92
efrechon <37827388+efrechon@users.noreply.github.com>
2018-04-04 23:17:53 +0200

    Revert "Organisation"
    This reverts commit 63979288c74e258a9853b35b943970c534a7b016.

---
+++ Trainning/AccueilType/iGAPET.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="iGAPETCSS.css"/>
+    <title>iGAPET</title>
+</head>
+<body>
+    <div class="page">
+        <?php include 'header.php'?>
+        <?php include 'menu.php'?>
+        <div class="contain">
+        </div>
+    </div>
+</body>
+</html>
