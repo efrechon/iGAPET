666ac4b15bfa388ae0a5f2504daac3b3e6516ec1
efrechon <edgar.frechon@free.fr>
2018-04-08 20:27:04 +0200

    URL

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 //Index du site
 
 <?php
+    // Début de la session
     session_start();
+
+    // Connexion à la base de données
+
+
+    // Redirection en fonction de l'URL
+    if(!empty($_GET['']) AND is_file('controleurs/'.$_GET[''].'.php')){
+        // Controleur de la page souhaitée
+        include ('controleurs/c_'.$_GET[''].'.php');
+    }
+    else{
+        // Page à afficher si problème d'URL
+        include ('controleurs/c_connexion.php');
+    }
+
 ?>
