425c7d99536b1e867bda75378cca7ede42f46dd8
efrechon <edgar.frechon@free.fr>
2018-04-08 22:37:33 +0200

    URL Suite

---
+++ controleurs/c_erreur.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Controleur de la page d'erreur
+ */
+    echo 'Erreur 404'.'</br>';
+    echo "Problème d'URL, Veuillez réessayer !";
+?>
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
-//Index du site
-
 <?php
+/**
+ *  Index du site
+ */
+
     // Début de la session
     session_start();
 
@@ -8,13 +10,13 @@
 
 
     // Redirection en fonction de l'URL
-    if(!empty($_GET['']) AND is_file('controleurs/'.$_GET[''].'.php')){
+    if(!empty($_GET['page']) AND is_file('controleurs/'.$_GET['page'].'.php')){
         // Controleur de la page souhaitée
-        include ('controleurs/c_'.$_GET[''].'.php');
+        include ('controleurs/c_'.$_GET['page'].'.php');
     }
     else{
         // Page à afficher si problème d'URL
-        include ('controleurs/c_connexion.php');
+        include ('controleurs/c_erreur.php');
     }
 
 ?>
