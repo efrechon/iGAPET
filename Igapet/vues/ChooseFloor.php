312762a41a444fb476b7fbcbb5800b54e4e406de
gregoireFessard <37829621+gregoireFessard@users.noreply.github.com>
2018-04-09 10:46:31 +0200

    Add files via upload

---
+++ Igapet/vues/ChooseFloor.php
@@ -0,0 +1,59 @@
+<?php
+
+require('conf/Home.php');
+
+$houseid  ="";
+
+if (isset($_POST['HouseID'])) {
+    $houseid = $_POST['HouseID'];
+}
+?>
+
+<div style="width:300px;height:210px;">
+    <h3> Captor </h3>
+    <form action="AddToDatabase.php" method="post">
+        Name: <input type="text" name="Name"><br>
+        CaptorType: <input type="text" name="CaptorType"><br>
+        xPosition: <input type="text" name="xPosition"><br>
+        yPosition: <input type="text" name="yPosition"><br>
+        <select name="RoomID">
+            <?php
+            $sql = "SELECT * FROM rooms WHERE HouseID=".$houseid."";
+            $rooms = get($db,$sql);
+            foreach($rooms as $room)
+            {
+                echo '<option value ="'.$room['RoomID'].'"> '.$room['Name'].'</option>';
+            }
+
+            ?>
+        </select>
+		<input type="hidden" name="Table" value="captors">
+        <input type="submit">
+    </form>
+</div>
+
+<form action="DrawFloor.php" method="post">
+    <h4>Floor :</h4>
+    <select name="Floor">
+        <?php
+
+        $sql = "SELECT * FROM houses WHERE HouseID=".$houseid."";
+        $req = $db->prepare($sql);
+        $req->execute();
+
+        $houses = $req->fetchAll(PDO::FETCH_ASSOC);
+
+        //echo $houses;
+
+        for ($x = 1; $x <= $houses[0]['NumberOfFloor']; $x++) {
+            echo '<option value ='.$x.'> '.$x.' </option>';
+        }
+
+        ?>
+    </select>
+    <input type="hidden" name="HouseID" value=<?php echo $houseid; ?>>
+    <input type="submit">
+</form>
+<form action="Choose%20house.php" method="post">
+    <input type="submit" value="return">
+</form>
---
+++ Igapet/vues/menu.php
@@ -0,0 +1,20 @@
+
+<div class="leftgroup">
+	<a class="leftbutton" href="Igapet.php"> Accueil </a>
+	<a class="leftbutton" href="Igapet.php"> Vue d'ensemble </a>
+</div>
+<div class="leftgroup">
+	<a class="leftbutton" href="Igapet.php"> Vue d'ensemble </a>
+	<a class="leftbutton" href="Igapet.php"> Mes capteurs </a>
+	<a class="leftbutton" href="Igapet.php"> Mes actionneurs </a>
+	<a class="leftbutton" href="Igapet.php"> Scénarios </a>
+	<a class="leftbutton" href="Igapet.php"> Notifications </a>
+</div>
+<div class="leftgroup">
+	<a class="leftbutton" href="Igapet.php"> Gérer les sous-utilisateurs </a>
+	<a class="leftbutton" href="Igapet.php"> Gérer les maisons </a>
+	<a class="leftbutton" href="Igapet.php"> Informations </a>
+	<a class="leftbutton" href="Igapet.php"> SAV </a>
+</div>
+
+
