9794be712ff6d6b38f27926ba41ac31ba7a04555
gregoireFessard <37829621+gregoireFessard@users.noreply.github.com>
2018-04-09 16:13:08 +0200

    Update LoadRoom.php

--- Igapet/controls/LoadRoom.php
+++ Igapet/controls/LoadRoom.php
@@ -10,12 +10,12 @@ if (isset($_POST['Floor'])) {
 }
 $HouseID = 1;
 $Floor = 1;
-$sql = "SELECT * FROM rooms WHERE HouseID=$HouseID AND Floor=$Floor";
+$sql = "SELECT * FROM Rooms WHERE HouseID=$HouseID AND Floor=$Floor";
 $Rooms = get($db,$sql);
 $captorArray = array();
 foreach($Rooms as $Room)
 {
-    $sqlcap = "SELECT * FROM captor WHERE RoomID=".$Room["RoomID"]."";
+    $sqlcap = "SELECT * FROM Captors WHERE RoomID=".$Room["RoomID"]."";
     $captors =get($db,$sqlcap);
     $captorArray['.$RoomID.'] = array();
     foreach($captors as $captor)
