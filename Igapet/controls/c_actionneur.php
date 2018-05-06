a3102d520d9d7323ffafb585d6774fac92ec237a
efrechon <edgar.frechon@free.fr>
2018-05-06 19:12:25 +0200

    Page actionneur Tri par pièce !

--- Igapet/controls/c_actionneur.php
+++ Igapet/controls/c_actionneur.php
@@ -5,8 +5,21 @@ include('models/m_actionneur');
 function gestion_actionneur($db){
     if($_POST['triA'] == 'triPiece'){
         $idhome= $_POST['triA'];
-        $requeteTriPA= $db->query('SELECT Name FROM rooms WHERE HouseID=$idhome');
-        $triPA= $requeteTriPA->fetch();
+        $requeteTriPA= $db->query('SELECT Name,RoomID FROM rooms WHERE HouseID=$idhome');
+        while($triPA= $requeteTriPA->fetch()){
+            $idroom= $triPA['RoomID'];
+            echo'<fieldest>';
+            echo'<legend>'.$triPA['Name'].'</legend>';
+            $requeteTriPA2= $db->query('SELECT ActuatorTypeID, State FROM actuators WHERE RoomID=$idroom');
+            while($triPA2= $requeteTriPA2->fetch()){
+                $idactionneur= $triPA2['ActuatorTypeID'];
+                $requeteTriPA3= $db->query('SELECT ActuatorName, Unit FROM actuatortypes WHERE ActuatorID=$idactionneur');
+                while($triPA3= $requeteTriPA3->fetch()){
+                    echo '<div class="actionneur">'.$triPA3['ActuatorName'].'<br/>'.$triPA2['State'].$triPA3['Unit'].'</div>';
+                }
+            }
+        }
+        echo'</fieldest>';
     }
 }
 
--- Igapet/vues/v_actionneurs.php
+++ Igapet/vues/v_actionneurs.php
@@ -6,7 +6,7 @@
 <!-- Début du contenu de la page -->
 <?php ob_start(); ?>
 <div id="selection">
-    <form action="" method="post">
+    <form action="index.php?pageAction=actionneurs" method="post">
         <select name="triA">
             <option value="triPiece">Trier par pièce</option>
             <option value="triActionneur">Trier par actionneur</option>
@@ -26,7 +26,27 @@
     </form>
 </div>
 <div id="affichageActionneur">
-
+    <?php
+        $db=connexion_BDD();
+        if($_POST['triA'] == 'triPiece'){
+            $idhome= $_POST['whereA'];
+            $requeteTriPA= $db->query("SELECT Name,RoomID FROM rooms WHERE HouseID=$idhome");
+            while($triPA= $requeteTriPA->fetch()){
+                $idroom= $triPA['RoomID'];
+                echo'<fieldset>';
+                echo'<legend>'.$triPA['Name'].'</legend>';
+                $requeteTriPA2= $db->query("SELECT ActuatorTypeID, State FROM actuators WHERE RoomID=$idroom");
+                while($triPA2= $requeteTriPA2->fetch()){
+                    $idactionneur= $triPA2['ActuatorTypeID'];
+                    $requeteTriPA3= $db->query("SELECT ActuatorName, Unit FROM actuatortypes WHERE ActuatorTypeID=$idactionneur");
+                    while($triPA3= $requeteTriPA3->fetch()){
+                        echo '<div class="actionneur">'.$triPA3['ActuatorName'].'<br/>'.$triPA2['State'].$triPA3['Unit'].'</div>';
+                    }
+                }
+            echo'</fieldset>';
+            }
+        }
+    ?>
 </div>
 <!-- Fin & Affectation du contenu de la page -->
 <?php $contenu=ob_get_clean(); ?>
