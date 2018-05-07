923bf4fc5e4d764a1c924408243a46f4c53a8727
paul-garnier <37874997+paul-garnier@users.noreply.github.com>
2018-05-07 14:21:30 +0200

    Add files via upload

--- Igapet/vues/v_connexion.php
+++ Igapet/vues/v_connexion.php
@@ -11,21 +11,22 @@
     <div id=header>
         <h1>iGAPET</h1>
     </div>
+    <br/><p> Soyez capable de répondre &agrave; vos besoins du quotidien sans bouger de votre lit ! </p>
     <div id="contain">
+    
         <div id="connexion">
-            <h3>Connexion</h3><br/><br/>
+            <h2>Connexion</h2><br/><br/>
             <form action="index.php?pageAction=connexion&new=visit" method="post">
                 <label for="emailC">Email : </label>
                 <input type="text" name="emailC"><br/><br/>
                 <label for="passwordC">Mot de passe : </label>
                 <input type="password" name="passwordC"><br/><br/>
+               	<a>Mot de passe oublié</a><br/><br/>
                 <input type="submit" value="Se connecter"><a href='index.php?pageAction=accueil'></a>
             </form>
-            <br>
-            <a>Mot de passe oublié</a>
         </div>
         <div id="inscription">
-            <h3>Inscription</h3><br/><br/>
+            <h2>Inscription</h2><br/><br/>
             <form action='index.php?pageAction=inscription&type=utilisateur' method="post">
                 <label for="emailI">Email : </label>
                 <input type="email" name="emailI"><br/><br/>
@@ -35,9 +36,33 @@
                 <input type="password" name="passwordI"><br/><br/>
                 <label for="verifpasswordI">Confirmer votre mot de passe : </label>
                 <input type="password" name="verifpasswordI"><br/><br/>
-                <input type="checkbox" name="cguOk">J'accepte les Conditions Générales d'utilisation<br/><br/>
+                <input type="checkbox" name="cguOk">J'accepte les Conditions Générales d'Utilisation<br/><br/>
                 <input type="submit" value="S'inscrire"><a href='index.php?pageAction=accueil'></a>
             </form>
+        </div>
+        <div id="notreconcept">
+        	<h2>Découvrez notre Concept</h2><br/><br/>
+        		<div class="concept">
+       					 <a>Vita est illis semper in fuga uxoresque mercenariae conductae ad tempus ex pacto atque,
+        					ut sit species matrimonii, dotis nomine futura coniunx hastam et tabernaculum offert marito</a>
+        		</div>
+        
+       			<div class="concept">
+       					 <a>Vita est illis semper in fuga uxoresque mercenariae conductae ad tempus ex pacto atque,
+       						ut sit species matrimonii, dotis nomine futura coniunx hastam et tabernaculum offert marito</a>
+       			</div>
+       			
+           		<div class="concept">
+       					 <a>Vita est illis semper in fuga uxoresque mercenariae conductae ad tempus ex pacto atque,
+        					ut sit species matrimonii, dotis nomine futura coniunx hastam et tabernaculum offert marito</a>
+        		</div>
+        
+       			<div class="concept">
+       					 <a>Vita est illis semper in fuga uxoresque mercenariae conductae ad tempus ex pacto atque,
+       						ut sit species matrimonii, dotis nomine futura coniunx hastam et tabernaculum offert marito</a>
+       			</div>             
+
+               
         </div>
     </div>
 </div>
